fb6feba04eee15932151002b29df20ffaec4845b
Matthew Millington <matthew.millington@stonybrook.edu>
2022-11-07 10:49:10 -0500

    create list and minor changes

---
+++ server/auth/index.js
@@ -0,0 +1,59 @@
+const jwt = require("jsonwebtoken")
+
+function authManager() {
+    verify = (req, res, next) => {
+        console.log("req: " + req);
+        console.log("next: " + next);
+        console.log("Who called verify?");
+        try {
+            const token = req.cookies.token;
+            if (!token) {
+                return res.status(401).json({
+                    loggedIn: false,
+                    user: null,
+                    errorMessage: "Unauthorized 1"
+                })
+            }
+
+            const verified = jwt.verify(token, process.env.JWT_SECRET)
+            console.log("verified.userId: " + verified.userId);
+            req.userId = verified.userId;
+
+            next();
+        } catch (err) {
+            console.error(err);
+            console.log(req.cookies)
+            return res.status(401).json({
+                loggedIn: false,
+                user: null,
+                errorMessage: "Unauthorized 2" + req.cookies.token
+            });
+        }
+    }
+
+    verifyUser = (req) => {
+        try {
+            const token = req.cookies.token;
+            if (!token) {
+                return null;
+            }
+
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            return decodedToken.userId;
+        } catch (err) {
+            return null;
+        }
+    }
+
+    signToken = (userId) => {
+        return jwt.sign({
+            userId: userId
+        }, process.env.JWT_SECRET);
+        
+    }
+
+    return this;
+}
+
+const auth = authManager();
+module.exports = auth;
